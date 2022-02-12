c5ee3cdfb11c66e2c39da4325b7d5019b7ea0fbf
Akshay Arora <akshay10103401@gmail.com>
2022-02-12 17:06:35 +0530

    Binary tree insertion

---
+++ Binary Tree/Insertion in Binary Tree.cs
@@ -0,0 +1,89 @@
+﻿namespace Binary_Tree
+{
+    class BinaryTree
+    {
+        public class Node
+        {
+            public int key;
+            public Node? left, right;
+
+            public Node(int key)
+            {
+                this.key = key;
+                left = null;
+                right = null;
+            }
+        }
+
+        static Node? root;
+
+        static void inorder(Node? temp)
+        {
+            if (temp == null)
+                return;
+
+            inorder(temp.left);
+            Console.Write(temp.key + " ");
+            inorder(temp.right);
+        }
+
+        static void insert(Node temp, int key)
+        {
+            if(temp == null)
+            {
+                root = new Node(key);
+                return;
+            }
+            
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(temp);
+
+            // Do level order traversel until we find an empty place for new key
+            while(q.Count != 0)
+            {
+                temp = q.Peek();
+                q.Dequeue();
+
+                if(temp.left == null)
+                {
+                    temp.left = new Node(key);
+                    break;
+                }
+                else
+                {
+                    q.Enqueue(temp.left);
+                }
+
+                if(temp.right == null)
+                {
+                    temp.right = new Node(key);
+                    break;
+                }
+                else
+                {
+                    q.Enqueue(temp.right);
+                }
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            root = new Node(10);
+
+            insert(root, 20);
+            inorder(root);
+
+            Console.WriteLine();
+            insert(root, 30);
+            inorder(root);
+
+            Console.WriteLine();
+            insert(root, 40);
+            inorder(root);
+
+            Console.WriteLine();
+            insert(root, 50);
+            inorder(root);
+        }
+    }
+}
