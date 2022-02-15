d745a7571ecef5284bbc1863f3f523caf6da2976
Akshay Arora <akshay10103401@gmail.com>
2022-02-15 15:15:22 +0530

    Added diameter of binary tree with the longest path as follow up question
    Added 2 ways of finding the diameter or tree and follow-up -> longest path in a binary tree.

---
+++ Binary Tree/Diameter Of Binary Tree.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree
+{
+    internal class Diameter_Of_Binary_Tree
+    {
+		class Node
+		{
+			int val;
+			public Node? left;
+			public Node? right;
+
+			public Node(int val)
+			{
+				this.val = val;
+				left = null;
+				right = null;
+			}
+		}
+
+		class BinaryTree
+		{
+			public Node? root;
+
+			/**
+			 * slow solution
+			 * finds longest path between two leaf nodes in a tree
+			 * even though this looks simple, since we call height
+			 * recursively makes this solution quadratic
+			 * Runtime: O(n^2)
+			 * @param node
+			 * @return
+			 */
+			public int findDiameter(Node? node)
+			{
+				if (node == null)
+					return 0;
+
+				int d1 = findDiameter(node.left);
+				int d2 = findDiameter(node.right);
+
+				int h1 = height(node.left);
+				int h2 = height(node.right);
+
+				return Math.Max(h1 + h2 + 1, Math.Max(d1, d2));
+			}
+
+			/**
+			 * find height recursively
+			 * runtime: O(n)
+			 * @param node
+			 * @return
+			 */
+			public int height(Node? node)
+			{
+				if (node == null)
+					return 0;
+				return Math.Max(height(node.left), height(node.right)) + 1;
+			}
+
+			/**
+			 * Best solution
+			 * solution that finds height and diameter for each node for
+			 * only that level so it does not involve double looping like 
+			 * in the other solution making this a linear time approach
+			 * Runtime: O(n)
+			 * @param node
+			 * @param height
+			 * @return
+			 */
+			public int findDiameterWithHeight(Node? node, Height height)
+			{
+				if (node == null)
+					return 0;
+				Height lh = new Height();
+				Height rh = new Height();
+
+				int d1 = findDiameterWithHeight(node.left, lh);
+				int d2 = findDiameterWithHeight(node.right, rh);
+
+				height.h = Math.Max(lh.h, rh.h) + 1;
+				return Math.Max(lh.h + rh.h + 1, Math.Max(d1, d2));
+			}
+		}
+
+		// needed to maintain height states in between calls
+		class Height
+		{
+			public int h;
+		}
+
+		public static void Main(String[] args)
+		{
+			BinaryTree bt = new BinaryTree();
+			bt.root = new Node(1);
+			bt.root.left = new Node(2);
+			bt.root.right = new Node(3);
+			bt.root.left.left = new Node(4);
+			bt.root.left.right = new Node(5);
+			Console.WriteLine("Diameter: " + bt.findDiameter(bt.root));
+			Console.WriteLine("Diameter with height method: " + bt.findDiameterWithHeight(bt.root, new Height()));
+		}
+	}
+}
+
---
+++ Binary Tree/Diameter-2(optimized) with path.cs
@@ -0,0 +1,93 @@
+﻿/// <summary>
+/// Diameter of the tree is the longest path between any 2 nodes in a binary tree
+/// 
+/// Here we are recursively checking for left and right subtree diamter and keeping track of the current
+/// diameter in order to update the largest.
+/// 
+/// Additionally, follow-up question is print the longest path of the binary tree
+/// </summary>
+namespace Binary_Tree
+{
+    class Diameter_2_optimized__with_path
+    {
+        public class TreeNode
+        {
+            public int data;
+            public TreeNode? left;
+            public TreeNode? right;
+            public TreeNode(int data)
+            {
+                this.data = data;
+                left = null;
+                right = null;
+            }
+        }
+
+        public class Diameter
+        {
+            TreeNode? node;
+            static List<TreeNode>? maxPath;
+            static int diameter = 0;
+            
+            public static int findDiameter(TreeNode? root, ref int diameter)
+            {
+                if (root == null)
+                    return 0;
+
+                int left = findDiameter(root.left, ref diameter);
+                int right = findDiameter(root.right, ref diameter);
+
+                // update the diameter if left path + right path is larger
+                diameter = Math.Max(diameter, left + right);
+
+                // return the longest one between left path and right path
+                // we are adding one for the path connecting the node and its parent
+                return Math.Max(left, right) + 1;
+            }
+
+            public static List<TreeNode> MaxPath(TreeNode? root)
+            {
+                if (root == null)
+                    return new List<TreeNode>();
+
+                List<TreeNode> leftPath = MaxPath(root.left);
+                List<TreeNode> rightPath = MaxPath(root.right);
+
+                if(leftPath.Count + rightPath.Count > diameter)
+                {
+                    maxPath = new List<TreeNode>(leftPath);
+                    maxPath.Add(root);
+                    maxPath.AddRange(rightPath); // starting adding in last
+                    diameter = leftPath.Count + rightPath.Count;
+                }
+
+                List<TreeNode> path = new List<TreeNode>(leftPath.Count > rightPath.Count ? leftPath : rightPath);
+
+                path.Add(root);
+                return path;
+            }
+
+            public static void Main(string[] args)
+            {
+                Diameter d = new Diameter();
+                d.node = new TreeNode(1)
+                {
+                    left = new TreeNode(2),
+                    right = new TreeNode(3)
+                };
+                d.node.left.left = new TreeNode(4);
+                d.node.left.right = new TreeNode(5);
+
+                int diameter = Int32.MinValue;
+                Console.WriteLine("Diameter: {0}", findDiameter(d.node, ref diameter));
+
+                MaxPath(d.node);
+
+                foreach(var node in maxPath)
+                {
+                    Console.Write($"{node.data} ");
+                }
+            }
+        }
+    }
+}
