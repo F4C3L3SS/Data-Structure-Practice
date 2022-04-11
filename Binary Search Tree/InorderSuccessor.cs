bef5e582d4043aaa4f2f920a158aa457eb0ca914
Akshay Arora <akshay10103401@gmail.com>
2022-04-11 20:16:53 +0530

    Update InorderSuccessor.cs
    Updated the inorder successor of BST with inorder predecessor and their time & space complexity.

--- Binary Search Tree/InorderSuccessor.cs
+++ Binary Search Tree/InorderSuccessor.cs
@@ -1,8 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/// <summary>
+/// File contents:
+/// 1. Inorder Successor
+/// 2. Inorder Predecessor
+/// 
+/// Brute Force approach: 
+/// Store the inorder traversal and find the next greater value of the given node.
+/// Time Complexity: O(N), Space Complexity: O(N)
+/// 
+/// Below Approach:
+/// Time Complexity: O(H), H -> height of the BST
+/// Space Complexity: O(1), ignoring the recursion stack space
+/// 
+/// </summary>
 
 namespace Binary_Search_Tree
 {
@@ -43,24 +52,40 @@ namespace Binary_Search_Tree
 
             public TreeNode? inorderSuccessor(TreeNode? root, TreeNode? p)
             {
-                if (p?.right != null)
-                    return FindMin(p.right);
-
-                TreeNode? ans = null;
+                TreeNode? successor = null;
 
                 while(root != null)
                 {
-                    if(p.data < root.data)
+                    if(root.data <= p.data)
                     {
-                        ans = root;
+                        root = root.right;
+                    } 
+                    else
+                    {
+                        successor = root;
+                        root = root.left;
+                    }
+                }
+                return successor;
+            }
+
+            public TreeNode? inorderPredecessor(TreeNode? root, TreeNode? p)
+            {
+                TreeNode? predecessor = null;
+
+                while(root != null)
+                {
+                    if(root.data >= p.data)
+                    {
                         root = root.left;
                     }
                     else
                     {
+                        predecessor = root;
                         root = root.right;
                     }
                 }
-                return ans;
+                return predecessor;
             }
 
             public TreeNode? FindMin(TreeNode? root)
@@ -92,6 +117,17 @@ namespace Binary_Search_Tree
                 else
                 {
                     Console.WriteLine("Inorder successor does not exist.");
+                }
+
+                TreeNode? predecessor = tree.inorderPredecessor(root, temp);
+
+                if (predecessor != null)
+                {
+                    Console.WriteLine("Inorder predecessor of " + temp?.data + " is " + predecessor.data);
+                }
+                else
+                {
+                    Console.WriteLine("Inorder predecessor does not exist.");
                 }
             }
         }
