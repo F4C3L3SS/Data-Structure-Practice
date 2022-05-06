8b97f569e76b0fc6e7556af2417b236c13a42473
Akshay Arora <akshay10103401@gmail.com>
2022-05-06 14:38:13 +0530

    Added house robber 1 (LC 198) & house robber 2 (LC 213)
    Added house robber 1 (LC 198) & house robber 2 (LC 213)

---
+++ Dynamic Programming/House Robber 1.cs
@@ -0,0 +1,95 @@
+﻿/// <summary>
+/// LC 198
+/// 
+/// Brute Force Approach:
+/// look for all combinations in which you can chose to commit the robbery
+/// time complexity: O(N^2)
+/// space complexity: O(N)
+/// 
+/// Optimal Approach: 
+/// we have pick or non-pick choices for all the houses
+/// start with first index (0th)
+/// pick it -> add the value and add index + 2, (we cannot pick adjacent)
+/// non-pick -> just increment the index + 1
+/// TC: O(N)
+/// Space Complexity: O(N) + O(N) [recursion stack space + cache space]
+/// 
+/// optimized tabulation approach:
+/// we optimize space, instead of using the array,
+/// we use 2 variables to store previous 2 values only
+/// and compute further results
+/// TC: O(N)
+/// Space complexity: O(1)
+/// </summary>
+namespace Dynamic_Programming
+{
+    internal class House_Robber
+    {
+        private static int[]? cache;
+
+        public static int Rob(int[] nums)
+        {
+            cache = new int[nums.Length];
+            //return Func(0, nums);
+            //return Tabulation(nums);
+            return optimizedTabulation(nums);
+        }
+
+        public static int Func(int i, int[] nums)
+        {
+            if (i >= nums.Length)
+                return 0;
+
+            if (cache[i] != 0)
+                return cache[i];
+
+            int pick = nums[i] + Func(i + 2, nums);
+            int notPick = Func(i + 1, nums);
+
+            cache[i] = Math.Max(pick, notPick);
+            return cache[i];
+        }
+
+        public static int Tabulation(int[] nums)
+        {
+            int[] dp = new int[nums.Length + 1];
+            dp[nums.Length] = 0;
+            dp[nums.Length - 1] = nums[nums.Length - 1];
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                int pick = nums[i] + dp[i + 2];
+                int notPick = dp[i + 1];
+                dp[i] = Math.Max(pick, notPick);
+            }
+
+            return dp[0];
+        }
+
+        // .   .  .
+        // i-2 i-1 i , at one step i, we only need 2 prev values,
+        // pr2 pr  cur
+        public static int optimizedTabulation(int[] nums)
+        {
+            int prev2 = 0;
+            int prev = nums[nums.Length - 1];
+
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                int pick = nums[i] + prev2;
+                int notPick = prev;
+
+                int cur = Math.Max(pick, notPick);
+                prev2 = prev;
+                prev = cur;
+            }
+
+            return prev;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 7, 9, 3, 1 };
+            Console.WriteLine("Maximum money robbed: {0}", Rob(nums));
+        }
+    }
+}
---
+++ Dynamic Programming/House Robber 2.cs
@@ -0,0 +1,69 @@
+﻿/// <summary>
+/// LC 213, House Robber 2 (cannot pick adjacent and last & first house are adjacent)
+/// </summary>
+namespace Dynamic_Programming
+{
+    internal class House_Robber_2
+    {
+        public static int rob(int[] nums)
+        {
+            int n = nums.Length;
+            // Handle the case with the single house
+            if (n == 1) return nums[0];
+
+            int[] temp1 = new int[n];
+            int[] temp2 = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                if (i != 0) temp1[i] = nums[i];
+                if (i != n - 1) temp2[i] = nums[i];
+            }
+
+            //return Math.Max(solve(temp1), solve(temp2));
+
+            return Math.Max(optimizeSolve(temp1), optimizeSolve(temp2));
+        }
+
+        // Consumes the house robber 1 solution only
+        public static int solve(int[] nums)
+        {
+            int[] dp = new int[nums.Length + 1];
+            dp[nums.Length] = 0;
+            dp[nums.Length - 1] = nums[nums.Length - 1];
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                int pick = nums[i] + dp[i + 2];
+                int notPick = dp[i + 1];
+                dp[i] = Math.Max(pick, notPick);
+            }
+
+            return dp[0];
+        }
+
+        public static int optimizeSolve(int[] nums)
+        {
+            int n = nums.Length;
+            int prev2 = 0;
+            int prev = nums[n - 1];
+
+            for(int i = n - 2; i >= 0; i--)
+            {
+                int pick = nums[i] + prev2;
+                int notPick = prev;
+
+                int cur = Math.Max(pick, notPick);
+                prev2 = prev;
+                prev = cur;
+            }
+            return prev;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            int[] nums = new int[] { 6, 6, 4, 8, 4, 3, 3, 10 };
+            Console.WriteLine("Maximum robbed money: {0}", rob(nums));
+        }
+    }
+}
