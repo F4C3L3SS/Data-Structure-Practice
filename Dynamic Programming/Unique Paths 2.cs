ef2aed07d1037e30fb1c062e36ef1ae5e68a4461
Akshay Arora <akshay10103401@gmail.com>
2022-05-10 18:20:30 +0530

    Added LC 62,63 Unique Paths 1-2

---
+++ Dynamic Programming/Unique Paths 2.cs
@@ -0,0 +1,81 @@
+﻿/// <summary>
+/// LC - 63: Unique Paths 2
+/// 
+/// TC/ SC: same as Unique Paths 1
+/// </summary>
+namespace Dynamic_Programming
+{
+    internal class Unique_Paths_2
+    {
+        private static int[,] dp;
+        public static int func(int[][] grid, int m, int n)
+        {
+            //return countPaths(grid, m - 1, n - 1);
+
+            //dp = new int[m, n];
+            //return dpApp(grid, m - 1, n - 1);
+
+            return tabulationApproach(grid, m-1, n-1);
+        }
+
+        public static int countPaths(int[][] grid, int m, int n)
+        {
+            if (m >= 0 && n >= 0 && grid[m][n] == 1) return 0;
+            if (m == 0 && n == 0) return 1;
+            if (m < 0 || n < 0) return 0;
+
+            int up = countPaths(grid, m - 1, n);
+            int left = countPaths(grid, m, n - 1);
+            return up + left;
+        }
+
+        public static int dpApp(int[][] grid, int m, int n)
+        {
+            if (m >= 0 && n >= 0 && grid[m][n] == 1) return 0;
+            if (m == 0 && n == 0) return 1;
+            if (m < 0 || n < 0) return 0;
+
+            if (dp[m, n] != 0)
+                return dp[m, n];
+
+            int up = dpApp(grid, m - 1, n);
+            int left = dpApp(grid, m, n - 1);
+            return dp[m, n] = up + left;
+        }
+
+        public static int tabulationApproach(int[][] grid, int m, int n)
+        {
+            int[,] dpA = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[m][n] == 1)
+                        dpA[i, j] = 0;
+                    else if (i == 0 || j == 0)
+                        dpA[i, j] = 1;
+                    else
+                    {
+                        int up = dpA[i - 1, j];
+                        int left = dpA[i, j - 1];
+                        dpA[i, j] = up + left;
+                    }
+                }
+            }
+            return dpA[m - 1, n - 1];
+        }
+
+        public static void Main(string[] args)
+        {
+            int row = 3, col = 3;
+            int[][] grid =
+            {
+                new int[] { 0,0,0 },
+                new int[] { 0,1,0 },
+                new int[] { 0,0,0 }
+            };
+            Console.WriteLine("Number of ways to reach the end of the maze: {0}", func(grid, row, col));
+        }
+    }
+}
---
+++ Dynamic Programming/Unique Paths.cs
@@ -0,0 +1,83 @@
+﻿/// <summary>
+/// LC: 62 - Unique Paths
+/// 
+/// Brute Force:
+/// Count all the paths
+/// 
+/// Dp Approach: 
+/// count the paths up and left (only 2 ways we can move in a cell)
+/// TC: O(2^m*n)
+/// SC: O(path length) 
+/// where path length = O((m-1) + (n - 1))
+/// 
+/// Tabulation approach
+/// TC: O(M*N)
+/// SC: O(M*N)
+/// </summary>
+
+namespace Dynamic_Programming
+{
+    internal class Unique_Paths
+    {
+        private static int[,] dp;
+        public static int func(int m, int n)
+        {
+            //return countPaths(m - 1, n - 1);
+
+            dp = new int[m, n];
+            return dpApp(m - 1, n - 1);
+
+            //return tabulationApproach(m, n);
+        }
+
+        public static int countPaths(int m, int n)
+        {
+            if (m == 0 && n == 0) return 1;
+            if (m < 0 || n < 0) return 0;
+
+            int up = countPaths(m - 1, n);
+            int left = countPaths(m, n - 1);
+            return up + left;
+        }
+
+        public static int dpApp(int m, int n)
+        {
+            if (m == 0 && n == 0) return 1;
+            if (m < 0 || n < 0) return 0;
+
+            if (dp[m, n] != 0)
+                return dp[m, n];
+
+            int up = dpApp(m - 1, n);
+            int left = dpApp(m, n - 1);
+            return dp[m,n] = up + left;
+        }
+
+        public static int tabulationApproach(int m, int n)
+        {
+            int[,] dp = new int[m, n];
+
+            for(int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if (i == 0 || j == 0)
+                        dp[i,j] = 1;
+                    else
+                    {
+                        int up = dp[i - 1, j];
+                        int left = dp[i, j - 1];
+                        dp[i, j] = up + left;
+                    }
+                }
+            }
+            return dp[m - 1, n - 1];
+        }
+
+        public static void Main(string[] args)
+        {
+            int row = 3, col = 3;
+            Console.WriteLine("Number of ways to reach the end of the maze: {0}", func(row, col));
+        }
+    }
+}
